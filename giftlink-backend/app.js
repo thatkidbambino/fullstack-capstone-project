292969ce6efd582197417d136e4da2fc14487a4e
thatkidbambino <matthewsolliday@yahoo.com>
2024-12-06 20:50:08 -0500

    Made adjustments to teh searchRoutes and app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -2,31 +2,36 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const pinoLogger = require('./logger');
+
 const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
+
 
 const app = express();
+app.use("*",cors());
 const port = 3060;
 
-// Middleware
-app.use(express.json());
-app.use("*", cors());
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
 
-// Connect to MongoDB
-connectToDatabase()
-    .then(() => {
-        console.log('Connected to DB');
-    })
-    .catch((e) => {
-        console.error('Failed to connect to DB', e);
-    });
+
+app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftRoutes
 const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+
+app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method
 app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -34,12 +39,10 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-// Default Route
-app.get("/", (req, res) => {
-    res.send("Inside the server");
-});
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
