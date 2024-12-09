7522f822e8064b6e3ecf91639e6cb8c95f3cc641
thatkidbambino <matthewsolliday@yahoo.com>
2024-12-09 17:57:25 -0500

    Changed SearchPage.js and SearchPage.css

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
+
 function App() {
   const navigate = useNavigate();
   return (
@@ -18,7 +20,8 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          
+          <Route path="/app/search" element={<SearchPage/>} />
+
         </Routes>
         </>
   );
